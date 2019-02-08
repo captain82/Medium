3e1a29a79034e03e6f115d2b65977410ee62a80f
Ankit <nktkumar82@gmail.com>
2019-02-08 23:41:10 +0530

    Initial commit

---
+++ app/src/main/java/com/captain/ak/medium/MainActivity.java
@@ -0,0 +1,100 @@
+package com.captain.ak.medium;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtView;
+    private NotificationReceiver nReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtView = (TextView) findViewById(R.id.textView);
+        nReceiver = new NotificationReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.captain.ak.medium.NOTIFICATION_LISTENER_EXAMPLE");
+        registerReceiver(nReceiver,filter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.settings:
+                Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                startActivity(intent);
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+
+        }
+
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nReceiver);
+    }
+
+
+
+    public void buttonClicked(View v){
+
+        if(v.getId() == R.id.btnCreateNotify){
+            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
+            ncomp.setContentTitle("My Notification");
+            ncomp.setContentText("Notification Listener Service Example");
+            ncomp.setTicker("Notification Listener Service Example");
+            ncomp.setSmallIcon(R.mipmap.ic_launcher_round);
+            ncomp.setAutoCancel(true);
+            nManager.notify((int)System.currentTimeMillis(),ncomp.build());
+        }
+        else if(v.getId() == R.id.btnClearNotify){
+            Intent i = new Intent("com.captain.ak.medium.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","clearall");
+            sendBroadcast(i);
+        }
+        else if(v.getId() == R.id.btnListNotify){
+            Intent i = new Intent("com.captain.ak.medium.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+            i.putExtra("command","list");
+            sendBroadcast(i);
+        }
+
+
+    }
+
+    class NotificationReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
+            txtView.setText(temp);
+        }
+    }
+}
